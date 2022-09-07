42b41b75c603ebce0db1cb69559357d686e99c9d
glinkinzhenya <glinkinzhenya@gmail.com>
2022-09-07 17:45:57 +0300

    version1.4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 $('.navbar-hamburger').click(function () {
   $(this).toggleClass('open');
-  $('.navbar__list').toggleClass('open');
+  // $('.navbar__list').toggleClass('open');
+
+  $('.navbar__list').stop(true, true).slideToggle(500);
+
 });
